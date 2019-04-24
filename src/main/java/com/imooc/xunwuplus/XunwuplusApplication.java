c460bc49d6487241597c966ded99b2d2a464c506
djz236 <djz236@163.com>
2019-04-24 10:44:17 +0800

    'create'

---
+++ src/main/java/com/imooc/xunwuplus/XunwuplusApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.xunwuplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XunwuplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XunwuplusApplication.class, args);
+	}
+
+}
